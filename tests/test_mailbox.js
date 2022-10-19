3ac392dac4b2967480f24c4e58a649c1dff0eaf0
Theo von Arx <theo.vonarx@xwiki.com>
2022-10-19 14:09:31 +0200

    Add mailbox tests

---
+++ tests/test_mailbox.js
@@ -0,0 +1,70 @@
+var Crypto = require("../crypto");
+var Nacl = require("tweetnacl");
+var test = require('tape'); 
+
+var Alice = Nacl.box.keyPair();
+var Alice_public = Nacl.util.encodeBase64(Alice.publicKey);
+
+var Bob = Nacl.box.keyPair();
+var Bob_public = Nacl.util.encodeBase64(Bob.publicKey);
+
+var Curve = Crypto.Curve;
+
+
+test('Test basic Mailbox encryption', function (t) {
+    var message = "test";
+
+    // Encrypt
+    var envelope = Crypto.Mailbox.sealSecretLetter(message, {
+        their_public: Alice.publicKey,
+
+        my_private: Bob.secretKey,
+        my_public: Bob.publicKey,
+    });
+    // Decrypt
+    var letter = Crypto.Mailbox.openSecretLetter(envelope, {
+        my_private: Alice.secretKey,
+    });
+
+    t.equal(message, letter.content, "Can decrypt own content");
+    t.equal(Bob_public, letter.author, "Author of own letter is correct");
+
+    t.end();
+});
+
+test('Test basic Mailbox encryption', function (t) {
+    // Bob wants to drop a letter in Alice' mailbox.
+
+    // Bob creates an encryptor with his base64-encoded private key
+    var bob_cryptor = Crypto.Mailbox.createEncryptor({
+        curvePrivate: Nacl.util.encodeBase64(Bob.secretKey),
+        curvePublic: Nacl.util.encodeBase64(Bob.publicKey),
+    });
+
+    // Bob writes his very important letter
+    var bob_plaintext = "pewpewpew bangbangbang";
+
+    var bob_ciphertext = bob_cryptor.encrypt(
+        bob_plaintext,
+        Alice_public // encrypt for Alice' base64-encoded public key
+    );
+
+    t.assert(Boolean(bob_ciphertext));
+    t.equal('string', typeof(bob_ciphertext));
+
+    // Alice checks her mailbox
+    var Alice_cryptor = Crypto.Mailbox.createEncryptor({
+        curvePrivate: Nacl.util.encodeBase64(Alice.secretKey),
+        curvePublic: Nacl.util.encodeBase64(Alice.publicKey),
+    });
+
+    var alice_plaintext = Alice_cryptor.decrypt(bob_ciphertext);
+
+    t.assert(Boolean(alice_plaintext));
+    t.equal('object', typeof(alice_plaintext));
+
+    t.equal(bob_plaintext, alice_plaintext.content, "Alice and Bob decrypt to the same message");
+
+    t.end();
+
+});
